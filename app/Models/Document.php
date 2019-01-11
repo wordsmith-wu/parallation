3c96a84e9b158718ff2fac5f69351149d1e821d6
wordsmith <wordsmith@foxmail.com>
2019-01-11 20:30:00 +0800

    Documents show page

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -1,8 +1,21 @@
 <?php
 
 namespace App\Models;
+use App\Models\User;
+use App\Models\Category;
+
 
 class Document extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'paragraphs_count', 'words_count', 'last_translator_id', 'order', 'slug'];
+
+    public function category()
+    {
+    	return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
 }
--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -1,60 +1,33 @@
 @extends('layouts.app')
 
+@section('title','文档列表')
+
 @section('content')
-<div class="container">
-  <div class="col-md-10 offset-md-1">
+
+<div class="row mb-5">
+  <div class="col-lg-9 col-md-9 document-list">
     <div class="card ">
-      <div class="card-header">
-        <h1>
-          Document
-          <a class="btn btn-success float-xs-right" href="{{ route('documents.create') }}">Create</a>
-        </h1>
+
+      <div class="card-header bg-transparent">
+        <ul class="nav nav-pills">
+          <li class="nav-item"><a class="nav-link active" href="#">最后翻译</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">最新发布</a></li>
+        </ul>
       </div>
 
       <div class="card-body">
-        @if($documents->count())
-          <table class="table table-sm table-striped">
-            <thead>
-              <tr>
-                <th class="text-xs-center">#</th>
-                <th>Title</th> <th>Body</th> <th>User_id</th> <th>Category_id</th> <th>Paragraphs_count</th> <th>Words_count</th> <th>Last_translator_id</th> <th>Order</th> <th>Slug</th>
-                <th class="text-xs-right">OPTIONS</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              @foreach($documents as $document)
-              <tr>
-                <td class="text-xs-center"><strong>{{$document->id}}</strong></td>
-
-                <td>{{$document->title}}</td> <td>{{$document->body}}</td> <td>{{$document->user_id}}</td> <td>{{$document->category_id}}</td> <td>{{$document->paragraphs_count}}</td> <td>{{$document->words_count}}</td> <td>{{$document->last_translator_id}}</td> <td>{{$document->order}}</td> <td>{{$document->slug}}</td>
-
-                <td class="text-xs-right">
-                  <a class="btn btn-sm btn-primary" href="{{ route('documents.show', $document->id) }}">
-                    V
-                  </a>
-
-                  <a class="btn btn-sm btn-warning" href="{{ route('documents.edit', $document->id) }}">
-                    E
-                  </a>
-
-                  <form action="{{ route('documents.destroy', $document->id) }}" method="POST" style="display: inline;" onsubmit="return confirm('Delete? Are you sure?');">
-                    {{csrf_field()}}
-                    <input type="hidden" name="_method" value="DELETE">
-
-                    <button type="submit" class="btn btn-sm btn-danger">D </button>
-                  </form>
-                </td>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
-          {!! $documents->render() !!}
-        @else
-          <h3 class="text-xs-center alert alert-info">Empty!</h3>
-        @endif
+        {{-- 文档列表 --}}
+        @include('documents._document_list', ['documents' => $documents])
+        {{-- 分页 --}}
+        <div class="mt-5">
+          {!! $documents->appends(Request::except('page'))->render() !!}
+        </div>
       </div>
     </div>
+  </div>
+
+  <div class="col-lg-3 col-md-3 sidebar">
+    @include('documents._sidebar')
   </div>
 </div>
 
