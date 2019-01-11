868bc91b4e1bdb5e10cc46e9cfbffa749bce02a9
wordsmith <wordsmith@foxmail.com>
2019-01-11 16:50:53 +0800

    Modify root page

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -2,5 +2,17 @@
 @section('title', '首页')
 
 @section('content')
-  <h1>这里是首页</h1>
-@stop
+  <div class="jumbotron">
+    <h1>Hello Parallation</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="{{route('root')}}">Parallation 翻译平台</a> 的主页。
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('register')}}" role="button">现在注册</a>
+    </p>
+  </div>
+  @stop
+
