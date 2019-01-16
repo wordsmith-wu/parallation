b168c4c3e41f0015802645daf4f2c27f171b1f8a
wordsmith <wordsmith@foxmail.com>
2019-01-16 20:06:42 +0800

    Restore

--- resources/views/documents/_sidebar.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="card ">
-  <div class="card-body">
-    <a href="{{ route('documents.create') }}" class="btn btn-success btn-block" aria-label="Left Align">
-      <i class="fas fa-pencil-alt mr-2"></i>  新建文件
-    </a>
-  </div>
-</div>
