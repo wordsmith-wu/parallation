83eb65276fefab6db805028d70c13aba9530c2c3
wordsmith <wordsmith@foxmail.com>
2019-01-16 19:56:35 +0800

    Finish

--- config/administrator.php
+++ config/administrator.php
@@ -21,7 +21,7 @@ return array(
      *
      * @type string
      */
-    'title' => config('app.name'),
+    'title' => config('app.name','Parallation'),
 
     /*
      * The path to your model config directory
